1f5003a6973b6f0b404ddb62f09c2a41bde1af70
Gabriel Borges <contato@gbdev.me>
2024-11-20 22:01:29 -0300

    feat: estrutura, component library, rotas públicas

---
+++ src/screens/sign-in.tsx
@@ -0,0 +1,77 @@
+import {
+  Center,
+  Heading,
+  Image,
+  Text,
+  VStack,
+  ScrollView,
+} from "@gluestack-ui/themed";
+
+import backgroundImg from "@assets/background.png";
+import Logo from "@assets/logo.svg";
+import { Input } from "@components/input";
+import { Button } from "@components/button";
+import { useNavigation } from "@react-navigation/native";
+import { AuthNavigatorRouterProps } from "@routes/auth.routes";
+
+export const SignIn = () => {
+  const navigation = useNavigation<AuthNavigatorRouterProps>();
+
+  const handleNewAccount = () => {
+    navigation.navigate("signUp");
+  };
+
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1}>
+        <Image
+          source={backgroundImg}
+          alt="Pessoas treinando"
+          w="$full"
+          height={624}
+          defaultSource={backgroundImg}
+          position="absolute"
+        />
+
+        <VStack flex={1} px="$10" pb="$16">
+          <Center my="$24">
+            <Logo />
+
+            <Text color="$gray100" fontSize="$sm">
+              Treine sua mente e seu corpo.
+            </Text>
+          </Center>
+
+          <Center gap="$2">
+            <Heading color="$gray100">Acesse a conta</Heading>
+
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+
+            <Input placeholder="Senha" secureTextEntry />
+
+            <Button title="Acessar" />
+          </Center>
+
+          <Center flex={1} justifyContent="flex-end" mt="$4">
+            <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
+              Ainda não tem acesso?
+            </Text>
+
+            <Button
+              title="Cadastre-se"
+              variant="outline"
+              onPress={handleNewAccount}
+            />
+          </Center>
+        </VStack>
+      </VStack>
+    </ScrollView>
+  );
+};
---
+++ src/screens/sign-up.tsx
@@ -0,0 +1,73 @@
+import {
+  Center,
+  Heading,
+  Image,
+  Text,
+  VStack,
+  ScrollView,
+} from "@gluestack-ui/themed";
+
+import backgroundImg from "@assets/background.png";
+import Logo from "@assets/logo.svg";
+import { Input } from "@components/input";
+import { Button } from "@components/button";
+import { useNavigation } from "@react-navigation/native";
+
+export const SignUp = () => {
+  const navigation = useNavigation();
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1}>
+        <Image
+          source={backgroundImg}
+          alt="Pessoas treinando"
+          w="$full"
+          height={624}
+          defaultSource={backgroundImg}
+          position="absolute"
+        />
+
+        <VStack flex={1} px="$10" pb="$16">
+          <Center my="$24">
+            <Logo />
+
+            <Text color="$gray100" fontSize="$sm">
+              Treine sua mente e seu corpo.
+            </Text>
+          </Center>
+
+          <Center gap="$2" flex={1}>
+            <Heading color="$gray100">Crie sua conta</Heading>
+
+            <Input placeholder="Nome" />
+
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+
+            <Input placeholder="Senha" secureTextEntry />
+
+            <Button title="Criar e acessar" />
+          </Center>
+
+          <Button
+            title="Voltar para o login"
+            variant="outline"
+            mt="$12"
+            onPress={handleGoBack}
+          />
+        </VStack>
+      </VStack>
+    </ScrollView>
+  );
+};
