582869b1938fbcc84222eb04b008daedb4961b06
Gabriel Borges <contato@gbdev.me>
2024-11-20 22:23:13 -0300

    feat: rotas privadas

---
+++ src/screens/exercise.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "@gluestack-ui/themed"
+
+export const Exercise = () => {
+  return (
+    <Center flex={1}>
+      <Text>
+        Exercise
+      </Text>
+    </Center>
+  )
+}
